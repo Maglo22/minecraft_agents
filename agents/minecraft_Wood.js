8c6f91e11626ba4cf0c53c5782bd2258f09aabe2
Manuel Flores <a01206898@itesm.mx>
2019-10-10 19:39:11 -0500

    updated agent

--- agents/minecraft_Wood.js
+++ agents/minecraft_Wood.js
@@ -6,6 +6,8 @@ const mcData=require("minecraft-data")("1.8.8")
 var blockFinderPlugin = require('mineflayer-blockfinder')(mineflayer);
 var navigatePlugin = require('mineflayer-navigate')(mineflayer);
 
+//All the axes possible
+var all_t = ['diamond_axe', 'iron_axe', 'golden_axe', 'stone_axe', 'wooden_axe']
 
 var bot = mineflayer.createBot({
   host: "localhost", // optional
@@ -70,6 +72,21 @@ bot.on('chat', function(username, message) {
 			if (blocks.length) {
 			  bot.navigate.to(blocks[0].position);
 			  bot.chat('I found Wood at ' + blocks[0].position + '.')
+			  
+			  //Here it equips the tool for the job
+			  for(var i=0; i < all_t.length; i++){
+				bot.chat('Entro for items' + all_t[i])
+				if (equipItem(all_t[i], 'hand')){
+					bot.chat('Entro a item true')
+					break;
+				}
+			  }
+			  
+				dig_front(blocks)
+			  
+			  //Minar el bloque de enfrente
+			  
+			  
 			  //bot.quit('quitting');
 			  return;
 			} else {
@@ -79,8 +96,99 @@ bot.on('chat', function(username, message) {
 			}
 		 });
 		break
+	case 'chop':
+		loop();
+		break
   }
 });
+
+function blockToHarvest () {
+  return bot.findBlock({
+    point: bot.entity.position,
+    matching: 17,
+  })
+}
+
+//funcion principal
+function loop(){
+	for(var a = 0; a <10; a++) {
+		bot.findBlock({
+			point: bot.entity.position,
+			matching: 17,
+			maxDistance: 50,
+			count: 1,
+		  }, function(err, blocks) {
+			if (err) {
+			  return bot.chat('Error trying to find Wood: ' + err);
+			  //bot.quit('quitting');
+			  return;
+			}
+			if (blocks.length) {
+			  bot.navigate.to(blocks[0].position);
+			  bot.chat('I found Wood at ' + blocks[0].position + '.')
+			  
+			  //Here it equips the tool for the job
+				equipItem(all_t[0], 'hand')
+			
+			  
+				dig_front(blocks)
+			  
+			  //Minar el bloque de enfrente
+			  
+			  
+			  //bot.quit('quitting');
+			  return;
+			} else {
+			  bot.chat("I couldn't find any Wood within 50.");
+			  //bot.quit('quitting');
+			  return;
+			}
+		 });
+	}
+}
+
+function dig_front(block){
+	var target = bot.blockAt(block[0].position)
+    if (target && bot.canDigBlock(target)) {
+      bot.chat(`starting to dig ${target.name}`)
+      bot.dig(target, onDiggingCompleted)
+    } else {
+      bot.chat('cannot dig')
+    }
+	
+	function onDiggingCompleted (err) {
+    if (err) {
+      console.log(err.stack)
+    }
+    bot.chat(`finished digging ${target.name}`)
+  }
+
+}
+
+function equipItem (name, destination) {
+    const item = itemByName(name)
+
+    if (item) {
+        bot.equip(item, destination, checkIfEquipped);
+		return true;
+    } 
+	else {
+		//bot.chat(`I have no ${name} to chop this down`);
+		return false;
+    }
+
+    function checkIfEquipped (err) {
+        if (err) {
+            bot.chat(`cannot equip ${name}: ${err.message}`);
+        } else {
+            //bot.chat(`equipped ${name}`);
+        }
+    }
+}
+
+function itemByName (name) {
+    return bot.inventory.items().filter(item => item.name === name)[0]
+}
 
 function sayItems (items = bot.inventory.items()) {
   const output = items.map(itemToString).join(', ')
